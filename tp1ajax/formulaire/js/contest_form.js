69503ef963d13255b16bbc4940e9f2629101741e
SylvainLangler <langlersylvain@gmail.com>
2019-11-14 15:57:53 +0100

    fix

--- tp1ajax/formulaire/js/contest_form.js
+++ tp1ajax/formulaire/js/contest_form.js
@@ -1,19 +1,20 @@
 
-/*$.getJSON( "../formulaire/choix_js.json", function( json ) {
+$.getJSON( "../formulaire/choix_js.json", function( json ) {
     for(let obj in json){
         document.getElementById("choix").innerHTML += '<option value="'+json[obj].label+'">'+json[obj].label+'</option>';
     }
-});*/
+});
 
-fetch('../formulaire/choix_js.json')
+/*fetch('../formulaire/choix_js.json')
     .then(function(response){
         console.log(response);
-    }); 
+    });
+*/ 
 
 $("#valider").click(function(){
 
     $.ajax({
-        type: 'POST',
+        method: 'POST',
         dataType: "json",
         url: "http://miw.spipha.fr:3000/api/contest_js",
         data: {
